7b08870c4c3d379a8499b3159a03ec280cefa72f
Khrystyna-Mia <grekulkristi@gmail.com>
2022-01-08 01:20:07 +0200

    08.01

---
+++ js/task-04.js
@@ -0,0 +1,31 @@
+// Счетчик состоит из спана и кнопок,
+// которые, при клике, должны увеличивать и уменьшать его значение на единицу.
+
+// 1. Создай переменную counterValue в которой будет храниться текущее значение счетчика и инициализируй её значением 0.
+// 2. Добавь слушатели кликов на кнопки, внутри которых увеличивай или уменьшай значение счтетчика.
+// 3. Обновляй интерфейс новым значением переменной counterValue.
+
+// const counterBtn = {
+//     subbtn: document.querySelector("[data-action='decrement']"),
+//     addbtn: document.querySelector("[data-action='increment']"),
+//     span: document.querySelector('#value'),
+// };
+
+
+// const counterValue = 0;
+
+// const decrement = () => {
+//     counterValue -= 1;
+
+//     counterBtn.span.textContent = counterValue;
+// };
+
+// const increment = () => {
+//     counterValue += 1;
+
+//     counterBtn.span.textContent = counterValue;
+
+// };
+
+// counterBtn.subbtn.addEventListener("click", decrement);
+// counterBtn.addbtn.addEventListener("click", increment);
