02e59d44a16a28a64e979c10208d3fb6704fc20b
Khrystyna-Mia <grekulkristi@gmail.com>
2022-01-14 23:05:50 +0200

    правка: data-length (согласно ТЗ)

--- js/task-06.js
+++ js/task-06.js
@@ -10,7 +10,7 @@ const input = document.getElementById("validation-input");
 input.onblur = function () {
     console.log(input.value.length);
     
-    if (input.getAttribute('data-length') < input.value.length) {
+    if (input.getAttribute('data-length') != input.value.length) {
         input.classList.remove('valid');
         input.classList.add('invalid');
     } else {
